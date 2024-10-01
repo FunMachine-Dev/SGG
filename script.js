eadaf9affa3cb45c6fc75c470d19e71f01d4ff79
FunMachine-dev <d.arriagadacampos@gmail.com>
2024-10-01 19:37:19 -0300

    front y script corregido

--- script.js
+++ script.js
@@ -41,19 +41,33 @@ document.addEventListener('DOMContentLoaded', function () {
     /*btn agregar gasto*/
     if (path.includes('reembolso.html')) {
         var btn_ingresar = document.getElementById('btn_ingresar');
-        var container = document.getElementById('container');
-        if (btn_ingresar && container) {
+        var datosIngresados = document.getElementById('datosIngresados');
+        if (btn_ingresar && datosIngresados) {
             let rowCounter = 0; // Contador para hacer únicos los name
+            let visibleGastos = 0; // Contador para gastos visibles
+            var container = document.createElement('fieldset');
 
             btn_ingresar.addEventListener('click', function () {
+
+                container.id = 'datosIngresadosHijo';
+                datosIngresados.appendChild(container);
+
+
                 // Incrementa el contador por cada fila nueva
                 rowCounter++;
+                visibleGastos++;
 
                 // Obtiene los valores
                 var fecha = document.getElementById('fecha').value;
                 var numBoleta = document.getElementById('num_boleta').value;
                 var descripcion = document.getElementById('descripcion').value;
                 var monto = document.getElementById('monto').value;
+
+                // Crear un elemento <hr> y título
+                var line = document.createElement('hr');
+                var tituloGasto = document.createElement('p');
+                tituloGasto.className = 'titulo-gasto';
+                tituloGasto.textContent = `Gasto ${visibleGastos}`;
 
                 // Crear una nueva fila
                 var row = document.createElement('div');
@@ -63,29 +77,49 @@ document.addEventListener('DOMContentLoaded', function () {
                 var deleteButton = document.createElement('div');
                 deleteButton.className = 'btn-delete';
                 deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M4 7l16 0" />
-            <path d="M10 11l0 6" />
-            <path d="M14 11l0 6" />
-            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-            </svg>`;
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M4 7l16 0" />
+                <path d="M10 11l0 6" />
+                <path d="M14 11l0 6" />
+                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg> 
+                <p class="delete">Eliminar gasto</p>`;
+
+                //acción del boton borrar
                 deleteButton.addEventListener('click', function () {
                     row.remove();
-                    updateTotal(); // Actualiza el total después de eliminar la fila
+                    tituloGasto.remove();
+                    line.remove();
+                    visibleGastos--;
+                    updateTotal();
+                    updateTituloGasto(); // Actualiza los totales después de eliminar la fila
                 });
 
-                // Insertar el botón de eliminar antes del primer campo de texto
-                row.appendChild(deleteButton);
+                // Insertar el conteo de gastos y el  botón de eliminar antes del primer campo de texto
+                // Crear un contenedor para el título y el botón de eliminar
+                var gastosDelete = document.createElement('div');
+                gastosDelete.className = 'tituloGastos';
+                gastosDelete.appendChild(tituloGasto);
+                gastosDelete.appendChild(deleteButton);
+                row.appendChild(gastosDelete);
+
 
                 // Crear 4 columnas, cada una con un campo de texto
+                var titles = ["Fecha", "N° boleta/factura", "Descripción", "Monto"];
                 var values = [fecha, numBoleta, descripcion, monto];
-                var names = [`fecha_${rowCounter}`, `num_boleta_${rowCounter}`, `descripcion_${rowCounter}`, `monto_${rowCounter}`]; // Nombres únicos
+                var names = [`fecha_${visibleGastos}`, `num_boleta_${visibleGastos}`, `descripcion_${visibleGastos}`, `monto_${visibleGastos}`]; // Nombres únicos
 
                 for (var i = 0; i < values.length; i++) {
                     var col = document.createElement('div');
-                    col.className = 'col';
+                    col.className = 'col-12 col-md-3';
 
+                    // Crear un título (label) para cada campo de texto
+                    var label = document.createElement('label');
+                    label.textContent = titles[i]; // Establecer el texto del título
+                    label.htmlFor = names[i]; // Asociar la etiqueta con el campo de texto
+
+                    //Crear campo de texto
                     var input = document.createElement('input');
                     input.className = 'input form-control agregar';
                     input.name = names[i]; // Asignar name único
@@ -95,7 +129,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         input.type = 'date'; // Primer campo como tipo date
                     } else if (i === values.length - 1) {
                         input.type = 'number'; // Último campo como tipo number para monto
-                        input.addEventListener('input', updateTotal); // Agregar evento input para actualizar total
+                        input.addEventListener('input', function () {
+                            updateTotal();
+                            updateDiferencia();
+                        }); // Agregar evento input para actualizar total
                     } else {
                         input.type = 'text'; // Campos intermedios como tipo text
                     }
@@ -107,12 +144,15 @@ document.addEventListener('DOMContentLoaded', function () {
                         input.value = values[i];
                     }
 
+                    col.appendChild(label);
                     col.appendChild(input);
                     row.appendChild(col);
+
                 }
 
                 // Agregar la fila al contenedor
                 container.appendChild(row);
+                container.appendChild(line);
 
                 // Borrar los datos de origen
                 document.getElementById('fecha').value = '';
@@ -122,13 +162,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 // Actualizar total
                 updateTotal();
+                
             });
+
+            // Función para actualizar los títulos de gastos
+            function updateTituloGasto() {
+                const titles = container.querySelectorAll('.titulo-gasto');
+                titles.forEach((title, index) => {
+                    title.innerText = `Gasto ${index + 1}`; // Actualiza el texto del título
+                });
+            }
 
             function formatNumber(value) {
                 // Remover cualquier carácter no numérico y agregar separadores de miles
                 return value.replace(/\D/g, '') // Elimina caracteres no numéricos
                     .replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Agrega separadores de miles
             }
+
+            
 
             function updateTotal() {
                 var total = 0;
